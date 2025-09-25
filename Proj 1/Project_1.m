28d41a27e26da275b6d8f5bd223cdab712d2b08a
Atticus... <atfr1899@colorado.edu>
2025-09-25 17:47:10 -0600

    did the graph for part b I hope

--- Proj 1/Project_1.m
+++ Proj 1/Project_1.m
@@ -1,5 +1,6 @@
 addpath('/Users/addi/Documents/MATLAB/APPM-2360/Proj 1');
 
+
 ti = 0;
 tf = 25;
 npts = 250;
@@ -12,11 +13,19 @@ disp t;
 disp w;
 
 
-%dT/dt = 0.25(75 − T ), T (0) = 50
+% dT/dt = 0.25(75 − T ), T (0) = 50
+% -ln|75-T| = 0.25t +C
+% 75 - T = ce^-0.25t
+% T= 75 - Ce^-0.25t 
+% 25 = C
+
+g = @(t) 75 - 25*exp(-0.25*t);
 
 
 %on the interval [0, 24] using 240 points (stepsize h = 0.1). 
 % Note that this is the same initial value problem as in Task Set A part 4(e).
 
 hold on;
-plot (t,w, "b-")
+plot (t,w, "r o")
+plot (t,g, "b-")
+hold off
