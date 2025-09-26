fcb739cb170aa78ad7a61124999de1cc1e0a3059
Atticus... <atfr1899@colorado.edu>
2025-09-26 11:13:50 -0600

    Added error to graph

--- Proj 1/Project_1.m
+++ Proj 1/Project_1.m
@@ -1,17 +1,19 @@
 addpath('/Users/addi/Documents/MATLAB/APPM-2360/Proj 1');
 
-
 ti = 0;
 tf = 25;
 npts = 250;
 y0 = 50;
+
+%functions
 f = @(t,T) 0.25*(75 - T);
 
+g = @(t) 75 - 25*exp(-0.25*t);
+
+u = g(t);
+
+%rkr call
 [t,w] = rk4(ti,tf,npts,y0,f);
-
-disp t;
-disp w;
-
 
 % dT/dt = 0.25(75 − T ), T (0) = 50
 % -ln|75-T| = 0.25t +C
@@ -19,13 +21,16 @@ disp w;
 % T= 75 - Ce^-0.25t 
 % 25 = C
 
-g = @(t) 75 - 25*exp(-0.25*t);
-
 
 %on the interval [0, 24] using 240 points (stepsize h = 0.1). 
 % Note that this is the same initial value problem as in Task Set A part 4(e).
 
 hold on;
 plot (t,w, "r o")
-plot (t,g, "b-")
+plot (t,g(t), "b-")
+
+plot (t,(u-w),"k--")
 hold off
+
+
+
