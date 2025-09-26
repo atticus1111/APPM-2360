355d68fe1bd5a85cdad06a0395a4097f20b4c306
Atticus... <atfr1899@colorado.edu>
2025-09-26 13:19:49 -0600

    trying to change the functions arround... currently not working

--- Proj 1/PartC.m
+++ Proj 1/PartC.m
@@ -3,14 +3,23 @@ ti = 0;
 tf = 24;
 npts = 240;
 y0 = 50;
-f = @(t,T) 0.25*(75 - T);
+
+function T = f(~,T)
+ 0.25*(75 - T);
+end
 
 [t,w] = rk4(ti,tf,npts,y0,f);
 
 %functions
-g = @(t) 75 - 25*exp(-0.25*t);
-u = g(t);
-dydt= @(t,y) 0.25*(75 - 12*cos(pi*(t-5)/12)-y);
+
+function T = g(t)
+ 75 - 25*exp(-0.25*t);
+end
+
+
+function dydt= h(t,y) 
+0.25*(75 - 12*cos(pi*(t-5)/12)-y);
+end
 
 [i,y]= ode45 (dydt,0:24,50);
 
